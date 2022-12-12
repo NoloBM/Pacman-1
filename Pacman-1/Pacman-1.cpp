63e2cc708fc824f503e75f018ae65c5b003e1a65
Portatil Nolo <periqui@noveas.com>
2022-12-12 12:51:01 +0100

    contar puntos mapa

--- Pacman-1/Pacman-1.cpp
+++ Pacman-1/Pacman-1.cpp
@@ -46,6 +46,18 @@ void Setup() {
 	map[11][MAP_HORIZONTAL - 1] = TILES::EMPTY;
 	map[12][MAP_HORIZONTAL - 1] = TILES::EMPTY;
 	map[13][MAP_HORIZONTAL - 1] = TILES::EMPTY;
+
+	for (size_t i = 0; i < MAP_VERTICAL; i++)
+	{
+		for (size_t j = 0; j < MAP_HORIZONTAL; j++)
+		{
+			if (map[i][j] == TILES::POINT) {
+				puntuacion_total++;
+			}
+			
+		}
+
+	}
 }
 
 void Input() {
@@ -138,6 +150,7 @@ void Draw() {
 		}
 		std::cout << std::endl;
 	}
+	std::cout << puntuacion_actual << '/' << puntuacion_total;
 }
 
 int main()
